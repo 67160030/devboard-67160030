b12b679ef427b173a3da91bcea243895d306faf0
Darunphop <67160030@go.buu.ac.th>
2026-03-27 21:50:17 +0700

    Activity43

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function NotFoundPage() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404</h1>
+      <p>ไม่พบหน้าที่คุณต้องการ</p>
+      <Link to="/">← กลับหน้าหลัก</Link>
     </div>
   );
 }
